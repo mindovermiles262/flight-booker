e7926391d4ec256d1ebc70a61d50292566643972
Andy Duss <mindovermiles262@gmail.com>
2017-08-06 13:56:04 -0700

    bookings styling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,23 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @passenger = Passenger.new
+    @flight = Flight.find(params[:selected])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      # Success
+    else
+      flash[:warning] = "Unable to Save Booking"
+      render 'bookings/new'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:selected, :passenger[:id, :name, :email])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'flights#index'
   resources :flights, only: [:index]
   resources :bookings
+  resources :passengers
   get 'flights', to: 'flights#index'
 end
