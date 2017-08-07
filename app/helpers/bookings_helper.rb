d7d9c34bf8815648e6e88c43b8a88636ca6bba21
Andy Duss <mindovermiles262@gmail.com>
2017-08-06 17:05:37 -0700

    refactor nested passenger form

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,6 @@
 module BookingsHelper
+  def setup_booking(booking)
+    booking.passenger ||= Passenger.new
+    booking
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,5 @@ class Booking < ApplicationRecord
   
   belongs_to :passenger
   belongs_to :flight
+  accepts_nested_attributes_for :passenger
 end
