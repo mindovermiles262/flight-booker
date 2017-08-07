9f7475970995fac0efb214d06bc9055a900b3fe2
Andy Duss <mindovermiles262@gmail.com>
2017-08-07 09:30:12 -0700

    fix styling, bookings all one one line

--- spec/factories/flights.rb
+++ spec/factories/flights.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :flight do
     sequence(:number)
-    departure FactoryGirl.create(:airport)
-    arrival FactoryGirl.create(:airport)
+    departure {FactoryGirl.create(:airport)}
+    arrival {FactoryGirl.create(:airport)}
     departure_time "2017-08-04 08:19:57"
   end
 end
