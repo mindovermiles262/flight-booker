e0418b2f27d3708bc1ae8a30130847c6adc5705d
Andy Duss <mindovermiles262@gmail.com>
2017-08-08 08:18:12 -0700

    working bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,22 +2,24 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @passenger = Passenger.new
-    @flight = Flight.find(params[:selected])
+    # @flight = Flight.find(params[:selected])
   end
 
   def create
     @booking = Booking.new(booking_params)
+
     if @booking.save
-      # Success
+      redirect_to @booking
     else
-      flash[:warning] = "Unable to Save Booking"
-      render 'bookings/new'
+      @message = "NOT WORKING"
     end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:selected, :passenger[:id, :name, :email])
+    params.require(:booking).permit(:flight_id, 
+                  {:passenger_attributes => [:id, :name, :email]})
   end
+
 end
