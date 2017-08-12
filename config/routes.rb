c6c4cc95228b5fe9ce8fe074c3d369b72888f257
Andy Duss <mindovermiles262@gmail.com>
2017-08-12 14:56:50 -0700

    restrict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'flights#index'
   resources :flights, only: [:index]
-  resources :bookings
-  resources :passengers
+  resources :bookings, only: [:new, :show, :create]
+  resources :passengers, only: [:new]
   get 'flights', to: 'flights#index'
   post 'bookings/new', to: 'bookings#create'
 end
