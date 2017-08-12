be651496f9c8568d57b8c5fac37d6c124f2d597b
Andy Duss <mindovermiles262@gmail.com>
2017-08-12 15:18:43 -0700

    passenger validations

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,17 @@
 class Passenger < ApplicationRecord
-  validates :name,  presence: true
-  validates :email, presence: true
+  validates :name,  presence: true, length: { maximum: 50 }
+  valid_email_address = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*[a-z]+\z/i
+  validates :email, presence: true, format: { with: valid_email_address },
+                    length: { maximum: 255 }
 
   belongs_to :booking, optional: true
   has_one :flight, through: :booking
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -19,5 +19,23 @@ RSpec.describe Passenger, type: :model do
       @passenger.email = ''
       expect(@passenger).to be_invalid
     end
+
+    it 'limits name length' do
+      @passenger.name = 'a' * 56
+      expect(@passenger).to be_invalid
+    end
+
+    it 'limits email length' do
+      @passenger.email = 'a' * 249 + "@ad.com"
+      expect(@passenger).to be_invalid
+    end
+
+    it 'filters invalid email addresses' do
+      invalids = %w[foo@example,com ad_at_ad.com foo@bar.baz@foo.com foo@bar..com]
+      invalids.each do |address|
+        @passenger.email = address
+        expect(@passenger).to be_invalid
+      end
+    end
   end
 end
